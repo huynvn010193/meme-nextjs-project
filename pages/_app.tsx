19c2419720b2a48dd7a7c76f30255bba9669fa33
huynvn010193 <hpn010193@gmail.com>
2021-04-22 08:27:47 +0700

    Done clu

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,21 @@
+import App, { AppContext, AppProps } from 'next/app';
 import { Header } from "../components/Header";
 import "../styles/globals.css";
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps, router }: AppProps) {
+
   return (
     <div id="root">
       <Header />
       <Component {...pageProps} />;
     </div>
   )
+}
+
+MyApp.getInitialProps = async (appContext: AppContext) => {
+  const appProps = await App.getInitialProps(appContext);
+  // Check gọi API để user login.
+  return { ...appProps }
 }
 
 export default MyApp;
