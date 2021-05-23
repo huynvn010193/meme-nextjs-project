62b92e4e95af679fb517af1fd5ec9f28361d150e
huynvn010193 <hpn010193@gmail.com>
2021-05-23 11:59:52 +0700

    Nhom chuc nang Authen - ThuThapDuLieuFormLogin

--- pages/login.tsx
+++ pages/login.tsx
@@ -13,6 +13,14 @@ const iniFormData: FormLoginData = {
 const Login = () => {
   const [formData, setFormData] = useState(iniFormData);
 
+  const handleOnChange = (key) => (evt: any) => {
+    const value = evt.target.value;
+    setFormData({
+      ...formData,
+      [key]: value,
+    });
+  };
+
   return (
     <div className="ass1-login">
       <div className="ass1-login__logo">
@@ -25,19 +33,21 @@ const Login = () => {
         <div className="ass1-login__form">
           <form action="#">
             <input
+              value={formData.email}
+              onChange={handleOnChange("email")}
               type="text"
               className="form-control"
               placeholder="Email"
               required
             />
-            <div className="ass1-input-copy">
-              <input
-                type="password"
-                className="form-control"
-                placeholder="Mật khẩu"
-                required
-              />
-            </div>
+            <input
+              value={formData.password}
+              onChange={handleOnChange("password")}
+              type="password"
+              className="form-control"
+              placeholder="Mật khẩu"
+              required
+            />
             <div className="ass1-login__send">
               <a href="dang-ky.html">Đăng ký một tài khoản</a>
               <button type="submit" className="ass1-btn">
